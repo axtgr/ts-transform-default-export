1d42b4df5a8e2f3d9371c09e8254fd02630c5f8f
Alex Schneider <me@schneider.ax>
2020-08-11 18:20:57 +0700

    It works

---
+++ src/index.ts
@@ -0,0 +1,134 @@
+import Path from 'path'
+import ts from 'typescript'
+
+function removeDefaultExportModifiers(modifiers: ts.ModifiersArray | ts.Modifier[]) {
+  return modifiers.filter(
+    (modifier) =>
+      modifier.kind !== ts.SyntaxKind.ExportKeyword &&
+      modifier.kind !== ts.SyntaxKind.DefaultKeyword
+  )
+}
+
+function hasDefaultExportModifiers(node: ts.Node) {
+  return (
+    node.modifiers &&
+    node.modifiers.find((m) => m.kind === ts.SyntaxKind.ExportKeyword) &&
+    node.modifiers.find((m) => m.kind === ts.SyntaxKind.DefaultKeyword)
+  )
+}
+
+function convertDeclaration(
+  node: ts.FunctionDeclaration | ts.ClassDeclaration,
+  addDeclare = false
+) {
+  let modifiers = node.modifiers ? removeDefaultExportModifiers(node.modifiers) : []
+
+  if (addDeclare) {
+    modifiers.unshift(ts.createModifier(ts.SyntaxKind.DeclareKeyword))
+  }
+
+  return {
+    ...node,
+    name: node.name || ts.getGeneratedNameForNode(node),
+    modifiers: (modifiers as unknown) as ts.ModifiersArray,
+  }
+}
+
+function createExportAssignmentForNode(node: ts.Node, isExportEquals = false) {
+  let name = ts.isIdentifier(node)
+    ? node
+    : (node as any).propertyName ||
+      (node as any).name ||
+      ts.getGeneratedNameForNode(node)
+  return ts.createExportAssignment(undefined, undefined, isExportEquals, name)
+}
+
+function visitor(
+  isDeclarationFile: boolean,
+  keepOriginalExports: boolean,
+  node: ts.Node
+): ts.Node | ts.Node[] | undefined {
+  if (ts.isExportDeclaration(node)) {
+    // `export { foo as default, bar, baz as qux }` → `export = foo`
+
+    let specifier = (node.exportClause as any)?.elements?.find(
+      (n: ts.Node) =>
+        ts.isExportSpecifier(n) && n.name.escapedText === 'default' && n.propertyName
+    )
+
+    if (!specifier) {
+      return node
+    }
+
+    let exportAssignment = createExportAssignmentForNode(specifier, true)
+
+    if (keepOriginalExports) {
+      return [node, exportAssignment]
+    }
+
+    return exportAssignment
+  }
+
+  if (
+    hasDefaultExportModifiers(node) &&
+    (ts.isFunctionDeclaration(node) || ts.isClassDeclaration(node))
+  ) {
+    // `export default function foo() {}` → `declare function foo(): void; export = foo`
+    // `export default class Bar {}` → `declare class Bar {}; export = Bar`
+
+    let nodes = [
+      convertDeclaration(node, isDeclarationFile),
+      createExportAssignmentForNode(node, true),
+    ]
+
+    if (keepOriginalExports) {
+      nodes.push(createExportAssignmentForNode(node))
+    }
+
+    return nodes
+  }
+
+  if (ts.isExportAssignment(node)) {
+    // `export default fn` → `export = fn`
+
+    let equalsAssignment = createExportAssignmentForNode(node.expression, true)
+
+    if (keepOriginalExports) {
+      return [node, equalsAssignment]
+    }
+
+    return equalsAssignment
+  }
+
+  return node
+}
+
+interface TransformerOptions {
+  keepOriginalExports?: boolean
+}
+
+function transformDefaultExport(
+  program: ts.Program,
+  options: TransformerOptions = {}
+): ts.TransformerFactory<ts.SourceFile> {
+  let rootFiles = program.getRootFileNames()
+  return (context) => {
+    return (file) => {
+      if (rootFiles.indexOf(Path.normalize(file.fileName)) === -1) {
+        return file
+      }
+
+      return ts.visitEachChild(
+        file,
+        visitor.bind(
+          null,
+          file.isDeclarationFile,
+          Boolean(options.keepOriginalExports)
+        ),
+        context
+      )
+    }
+  }
+}
+
+export { transformDefaultExport as default, TransformerOptions }
